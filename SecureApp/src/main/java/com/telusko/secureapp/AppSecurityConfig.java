3a510905baa138729be16bcaf0c33fe06072f718
NANDHINI RS <58160765+Nandhini31Rajesh@users.noreply.github.com>
2019-12-06 23:52:53 -0800

    Update AppSecurityConfig.java

--- SecureApp/src/main/java/com/telusko/secureapp/AppSecurityConfig.java
+++ SecureApp/src/main/java/com/telusko/secureapp/AppSecurityConfig.java
@@ -28,37 +28,3 @@ public class AppSecurityConfig extends WebSecurityConfigurerAdapter
 		//(new BCryptPasswordEncoder());
 		return provider;
 	}
-
-	//@Override
-	//protected void configure(HttpSecurity http) throws Exception {
-		
-		//http
-			//.csrf().disable()
-		//	.authorizeRequests().antMatchers("/login").permitAll()
-		//	.anyRequest().authenticated()
-		//	.and()
-		//	.formLogin()
-		//	.loginPage("/login").permitAll()
-		//	.and()
-		//	.logout().invalidateHttpSession(true)
-		//	.clearAuthentication(true)
-		//	.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-		//	.logoutSuccessUrl("/logout-success").permitAll();
-		
-	//}
-	
-	
-	
-//	@Bean
-//	@Override
-//	protected UserDetailsService userDetailsService() {
-//		
-//		
-//		List<UserDetails> users = new ArrayList<>();
-//		users.add(User.withDefaultPasswordEncoder().username("navin").password("1234").roles("USER").build());
-//		
-//		
-//		return new InMemoryUserDetailsManager(users);
-//	}
-	
-}
